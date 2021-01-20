ea1cf08c45eeb50dd43d0abe8c1039895dd1a689
Hugh Harper <hughaharper@gmail.com>
2021-01-20 11:00:10 -0800

    move headers and functions

--- src/02_wdr2cdr/die.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-/*---------------------------------------------------------------*/
-/* write out error message and exit                             */
-/* use two strings to allow more complicated error messages     */
-void die(char *s1, char *s2) {
-        fprintf(stderr, " %s %s \n", s1, s2);
-        exit(1);
-}
